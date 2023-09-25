2fc381ff3d5cccf5bfaaaf85e190f667eb72e65a
vishal <vishal88kh@gmail.com>
2023-09-25 16:57:39 +0530

    initial commit

---
+++ src/main/java/com/swapsell/swapsell/SwapSellApplication.java
@@ -0,0 +1,13 @@
+package com.swapsell.swapsell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapSellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapSellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swapsell/swapsell/SwapSellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapsell.swapsell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapSellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
