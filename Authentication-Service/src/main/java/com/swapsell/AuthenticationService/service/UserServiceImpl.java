0688d4c41474d77e00d2ab2f850c83ba8e95926d
vishal <vishal88kh@gmail.com>
2023-09-27 01:30:05 +0530

    added a UserServiceImpl class

---
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/service/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.swapsell.AuthenticationService.service;
+
+import com.swapsell.AuthenticationService.domain.User;
+import com.swapsell.AuthenticationService.exception.UserAlreadyExistsException;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User saveUser(User user) throws UserAlreadyExistsException {
+        return null;
+    }
+}
