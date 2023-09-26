6ec48fc4073fce3d44e0abb3d2b5ed57584de4af
vishal <vishal88kh@gmail.com>
2023-09-27 01:22:15 +0530

    extended JpaRepository

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/repository/UserRepository.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.swapsell.AuthenticationService.repository;
 
+import com.swapsell.AuthenticationService.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User,Long> {
 }
