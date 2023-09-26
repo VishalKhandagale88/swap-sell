2ff8db4e6320c96379dcf13b531c12ce1e4ae3ec
vishal <vishal88kh@gmail.com>
2023-09-27 01:29:31 +0530

    added a abstract method in UserService  class

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/service/UserService.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/service/UserService.java
@@ -1,4 +1,8 @@
 package com.swapsell.AuthenticationService.service;
 
+import com.swapsell.AuthenticationService.domain.User;
+import com.swapsell.AuthenticationService.exception.UserAlreadyExistsException;
+
 public interface UserService {
+    User saveUser(User user) throws UserAlreadyExistsException;
 }
