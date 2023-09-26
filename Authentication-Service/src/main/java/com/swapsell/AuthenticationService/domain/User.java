db5c50b228bc6c37d94b81b295d9bb8fbd437ba0
vishal <vishal88kh@gmail.com>
2023-09-26 12:21:25 +0530

    added fields and @Id and @GeneratedValue for User class

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/domain/User.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/domain/User.java
@@ -1,6 +1,10 @@
 package com.swapsell.AuthenticationService.domain;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
+
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -9,4 +13,11 @@ import lombok.*;
 @Setter
 @ToString
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long id;
+    private String fistName;
+    private String lastName;
+    private String email;
+    private String password;
 }
