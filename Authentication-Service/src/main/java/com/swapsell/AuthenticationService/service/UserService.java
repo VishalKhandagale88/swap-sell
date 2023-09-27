951801344525aa9db59a187c1d1d08729b7c3cc4
vishal <vishal88kh@gmail.com>
2023-09-27 18:16:45 +0530

    removed service annotation form UserService interface

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/service/UserService.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/service/UserService.java
@@ -4,7 +4,7 @@ import com.swapsell.AuthenticationService.domain.User;
 import com.swapsell.AuthenticationService.exception.UserAlreadyExistsException;
 import org.springframework.stereotype.Service;
 
-@Service
+
 public interface UserService {
     User saveUser(User user) throws UserAlreadyExistsException;
 }
