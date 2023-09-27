7075617e7b2a366d134fb22c36b50abb4f1cb46f
vishal <vishal88kh@gmail.com>
2023-09-27 17:52:00 +0530

    added @RestController annotation

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/controller/UserController.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/controller/UserController.java
@@ -2,7 +2,9 @@ package com.swapsell.AuthenticationService.controller;
 
 
 import com.swapsell.AuthenticationService.service.UserService;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class UserController {
     private UserService userService;
 
