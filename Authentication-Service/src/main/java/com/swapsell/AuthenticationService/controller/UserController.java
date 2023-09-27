997d5426e2112bdd97935c00f160236246d1c16f
vishal <vishal88kh@gmail.com>
2023-09-27 18:07:42 +0530

    added constructor

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/controller/UserController.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/controller/UserController.java
@@ -4,13 +4,19 @@ package com.swapsell.AuthenticationService.controller;
 import com.swapsell.AuthenticationService.domain.User;
 import com.swapsell.AuthenticationService.exception.UserAlreadyExistsException;
 import com.swapsell.AuthenticationService.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
+    @Autowired
     private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody User user){
