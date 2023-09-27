dc4e15af571faf6246ede256fb16fbd2de5c39e0
vishal <vishal88kh@gmail.com>
2023-09-27 21:11:48 +0530

    added a method to register user into data base

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/service/UserServiceImpl.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/service/UserServiceImpl.java
@@ -2,12 +2,20 @@ package com.swapsell.AuthenticationService.service;
 
 import com.swapsell.AuthenticationService.domain.User;
 import com.swapsell.AuthenticationService.exception.UserAlreadyExistsException;
+import com.swapsell.AuthenticationService.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
     @Override
     public User saveUser(User user) throws UserAlreadyExistsException {
-        return null;
+        Optional<User> userWithEmailId = userRepository.findByEmail(user.getEmail());
+        if (userWithEmailId.isPresent()){
+            throw new UserAlreadyExistsException("This user is already present");
+        }
+        return userRepository.save(user);
     }
 }
