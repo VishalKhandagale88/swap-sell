94682af446a025c2f0b53b6c8667ec35887c0d50
vishal <vishal88kh@gmail.com>
2023-09-27 21:34:38 +0530

    added @Table annotation

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/domain/User.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/domain/User.java
@@ -1,9 +1,6 @@
 package com.swapsell.AuthenticationService.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Data
@@ -12,6 +9,7 @@ import lombok.*;
 @Getter
 @Setter
 @Entity
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
