82cc7e9f69caa17eaf65a97e284fc3ed7406661e
vishal <vishal88kh@gmail.com>
2023-09-27 21:04:17 +0530

    added constructor in UserRepository and also added @Autowired annotation

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/service/UserServiceImpl.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/service/UserServiceImpl.java
@@ -3,13 +3,19 @@ package com.swapsell.AuthenticationService.service;
 import com.swapsell.AuthenticationService.domain.User;
 import com.swapsell.AuthenticationService.exception.UserAlreadyExistsException;
 import com.swapsell.AuthenticationService.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService {
-
+    @Autowired
     private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public User saveUser(User user) throws UserAlreadyExistsException {
         Optional<User> userWithEmailId = userRepository.findByEmail(user.getEmail());
