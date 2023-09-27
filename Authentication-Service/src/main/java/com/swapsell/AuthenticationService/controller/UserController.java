d1fc1be3945a0b8598cdc130c0fb7b90d0f1f0ed
vishal <vishal88kh@gmail.com>
2023-09-27 18:03:33 +0530

    added a method to register user in data base

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/controller/UserController.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/controller/UserController.java
@@ -1,11 +1,24 @@
 package com.swapsell.AuthenticationService.controller;
 
 
+import com.swapsell.AuthenticationService.domain.User;
+import com.swapsell.AuthenticationService.exception.UserAlreadyExistsException;
 import com.swapsell.AuthenticationService.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
     private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user){
+        try {
+            return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
+        } catch (UserAlreadyExistsException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
