a74126e7961b3922e2a9ede19e6d4c2db0bbc218
vishal <vishal88kh@gmail.com>
2023-09-27 17:59:51 +0530

    added web dependency

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/controller/UserController.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/controller/UserController.java
@@ -2,7 +2,7 @@ package com.swapsell.AuthenticationService.controller;
 
 
 import com.swapsell.AuthenticationService.service.UserService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
