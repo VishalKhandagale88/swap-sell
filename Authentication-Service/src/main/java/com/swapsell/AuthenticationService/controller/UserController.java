8823d7e6a8c91a3e3c09b40a871a5189499930b1
vishal <vishal88kh@gmail.com>
2023-09-27 17:48:04 +0530

    created a controller package and UserController java class

---
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.swapsell.AuthenticationService.controller;
+
+
+import com.swapsell.AuthenticationService.service.UserService;
+
+public class UserController {
+    private UserService userService;
+
+}
