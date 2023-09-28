350f359118595f6f7ce8152626135aeb446fc0bf
vishal <vishal88kh@gmail.com>
2023-09-28 19:20:28 +0530

    added userLogIn() method in controller

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/controller/UserController.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/controller/UserController.java
@@ -3,6 +3,7 @@ package com.swapsell.AuthenticationService.controller;
 
 import com.swapsell.AuthenticationService.domain.User;
 import com.swapsell.AuthenticationService.exception.UserAlreadyExistsException;
+import com.swapsell.AuthenticationService.exception.UserDoesNotExistsException;
 import com.swapsell.AuthenticationService.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,6 +24,17 @@ public class UserController {
         try {
             return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
         } catch (UserAlreadyExistsException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/logIn")
+    public ResponseEntity<?> userLogIn(@PathVariable String email){
+        try {
+            User user = userService.UserLogIn(email);
+            return new ResponseEntity<>(user,HttpStatus.OK);
+
+        } catch (UserDoesNotExistsException e) {
             throw new RuntimeException(e);
         }
     }
