78a0bbb3d11f16b6c82d7f036f244b35b63f5223
vishal <vishal88kh@gmail.com>
2023-09-29 02:43:33 +0530

    added lombok annotations

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/domain/UserResponse.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/domain/UserResponse.java
@@ -1,4 +1,14 @@
 package com.swapsell.AuthenticationService.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class UserResponse {
+
 }
