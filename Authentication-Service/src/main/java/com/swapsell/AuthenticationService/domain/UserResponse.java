f3a3f340847757cca6177f476a60c73272073d9c
vishal <vishal88kh@gmail.com>
2023-09-29 02:44:45 +0530

    added attributes for UserResponse.java

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/domain/UserResponse.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/domain/UserResponse.java
@@ -10,5 +10,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserResponse {
-
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
