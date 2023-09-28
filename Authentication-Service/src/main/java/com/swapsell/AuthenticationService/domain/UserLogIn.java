0fd68fd6df9950ce840413f0cc737264707f8c97
vishal <vishal88kh@gmail.com>
2023-09-29 03:03:06 +0530

    added a new UserLogIn class and added fields in it

---
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/domain/UserLogIn.java
@@ -0,0 +1,15 @@
+package com.swapsell.AuthenticationService.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLogIn {
+    private String email;
+    private  String password;
+}
--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/security/JWTSecurityTokenGenerator.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/security/JWTSecurityTokenGenerator.java
@@ -1,4 +1,7 @@
 package com.swapsell.AuthenticationService.security;
 
+import java.util.Map;
+
 public interface JWTSecurityTokenGenerator {
+
 }
