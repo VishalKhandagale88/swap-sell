7fe697466e2eaa3dfbdd4156c278d43ac154eaf6
vishal <vishal88kh@gmail.com>
2023-09-29 03:09:17 +0530

    added io.jsonwebtoken dependency

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/security/JWTSecurityTokenGeneratorImpl.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/security/JWTSecurityTokenGeneratorImpl.java
@@ -2,11 +2,13 @@ package com.swapsell.AuthenticationService.security;
 
 import com.swapsell.AuthenticationService.domain.UserLogIn;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class JWTSecurityTokenGeneratorImpl implements JWTSecurityTokenGenerator {
     @Override
     public Map<String, String> generateJWTToken(UserLogIn userLogIn) {
+        Map<String,String> token = new HashMap<>();
         return null;
     }
 }
