7b06135fd0fd08817962567964ce28fd2ccd6d32
vishal <vishal88kh@gmail.com>
2023-09-29 03:21:50 +0530

    added logic to generate a JWT token

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/security/JWTSecurityTokenGeneratorImpl.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/security/JWTSecurityTokenGeneratorImpl.java
@@ -2,6 +2,8 @@ package com.swapsell.AuthenticationService.security;
 
 import com.swapsell.AuthenticationService.domain.UserLogIn;
 import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,7 +12,14 @@ public class JWTSecurityTokenGeneratorImpl implements JWTSecurityTokenGenerator 
     @Override
     public Map<String, String> generateJWTToken(UserLogIn userLogIn) {
         Map<String,String> token = new HashMap<>();
-
-        return null;
+        String jwtToken = Jwts.builder()
+                .claim("email",userLogIn.getEmail())
+                .setSubject(userLogIn.getEmail())
+                .signWith(SignatureAlgorithm.HS256,"secretKey")
+                .compact();
+        token.put("token",jwtToken);
+        token.put("message","Logged in successfully");
+        token.put("email", userLogIn.getEmail());
+        return token;
     }
 }
