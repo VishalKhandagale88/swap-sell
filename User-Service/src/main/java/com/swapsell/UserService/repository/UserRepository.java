c040263694fb02aaaef7659386f7d9dae2d74392
vishal <vishal88kh@gmail.com>
2023-10-01 20:04:28 +0530

    declared a method findUserByEmail

--- User-Service/src/main/java/com/swapsell/UserService/repository/UserRepository.java
+++ User-Service/src/main/java/com/swapsell/UserService/repository/UserRepository.java
@@ -4,7 +4,11 @@ import com.swapsell.UserService.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User,Long> {
+
+    Optional<User> findUserByEmail(String email);
 
 }
