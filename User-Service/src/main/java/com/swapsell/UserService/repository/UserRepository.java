86075b690380045824207e7bd8f1f33422734162
vishal <vishal88kh@gmail.com>
2023-10-01 20:03:15 +0530

    annotated with @Repository

--- User-Service/src/main/java/com/swapsell/UserService/repository/UserRepository.java
+++ User-Service/src/main/java/com/swapsell/UserService/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.swapsell.UserService.repository;
 
 import com.swapsell.UserService.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends MongoRepository<User,Long> {
+
 }
