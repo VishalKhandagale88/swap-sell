b113f4be18a6995cc520bff4a837fc8b3c12c370
vishal <vishal88kh@gmail.com>
2023-10-01 16:50:54 +0530

    added fields in for product class

--- User-Service/src/main/java/com/swapsell/UserService/domain/Product.java
+++ User-Service/src/main/java/com/swapsell/UserService/domain/Product.java
@@ -11,5 +11,16 @@ import java.time.LocalDateTime;
 @Setter
 @EqualsAndHashCode
 public class Product {
-
+    private String name;
+    private String title;
+    private String description;
+    private byte image;
+    private double price;
+    private String category;
+    private String condition;
+    private int ageInDays;
+    private boolean exchangeable;
+    private String state;
+    private String city;
+    private LocalDateTime addPostedOnDate;
 }
