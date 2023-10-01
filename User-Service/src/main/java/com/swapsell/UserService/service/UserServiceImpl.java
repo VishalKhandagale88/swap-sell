152cd4f96883184fd2303483b1d0d788baf80570
vishal <vishal88kh@gmail.com>
2023-10-01 20:11:00 +0530

    UserServiceImpl is annotated with @Service

--- User-Service/src/main/java/com/swapsell/UserService/service/UserServiceImpl.java
+++ User-Service/src/main/java/com/swapsell/UserService/service/UserServiceImpl.java
@@ -2,7 +2,9 @@ package com.swapsell.UserService.service;
 
 import com.swapsell.UserService.domain.User;
 import com.swapsell.UserService.exception.UserAlreadyExistsException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
     @Override
     public User registerUserToApplication(User user) throws UserAlreadyExistsException {
