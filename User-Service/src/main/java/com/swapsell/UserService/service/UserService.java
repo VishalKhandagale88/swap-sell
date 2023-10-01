77b464f7d2f518cd26009ad568b6b864859336dc
vishal <vishal88kh@gmail.com>
2023-10-01 20:10:30 +0530

    implemented UserService method

--- User-Service/src/main/java/com/swapsell/UserService/service/UserService.java
+++ User-Service/src/main/java/com/swapsell/UserService/service/UserService.java
@@ -1,6 +1,8 @@
 package com.swapsell.UserService.service;
 
 import com.swapsell.UserService.domain.User;
+import com.swapsell.UserService.exception.UserAlreadyExistsException;
 
 public interface UserService {
+    User registerUserToApplication(User user) throws UserAlreadyExistsException;
 }
---
+++ User-Service/src/main/java/com/swapsell/UserService/service/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.swapsell.UserService.service;
+
+import com.swapsell.UserService.domain.User;
+import com.swapsell.UserService.exception.UserAlreadyExistsException;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User registerUserToApplication(User user) throws UserAlreadyExistsException {
+        return null;
+    }
+}
