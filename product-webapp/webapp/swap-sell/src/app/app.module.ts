122dfccbb0932b5e51f610b7af939a118e204963
vishal <vishal88kh@gmail.com>
2023-10-01 13:59:47 +0530

    added home component

--- product-webapp/webapp/swap-sell/src/app/app.module.ts
+++ product-webapp/webapp/swap-sell/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './features/login/login.component';
+import { HomeComponent } from './features/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
