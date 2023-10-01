af1c350c0aba83527a018901c2cad64fd78a63c4
vishal <vishal88kh@gmail.com>
2023-10-01 16:35:09 +0530

    added parent child relation by changing the parent tag of user-service

---
+++ User-Service/src/test/java/com/swapsell/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapsell.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
