bf8b6d2c967b16ef19ad8cf04737a651b5540ea6
vishal <vishal88kh@gmail.com>
2023-10-01 14:02:06 +0530

    added home componet as default component

--- product-webapp/webapp/swap-sell/src/app/app-routing.module.ts
+++ product-webapp/webapp/swap-sell/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
+import { HomeComponent } from './features/home/home.component';
 
 const routes: Routes = [
+  {
+    path:"",
+    component:HomeComponent
+  },
   {
     path:"login",
     component:LoginComponent
