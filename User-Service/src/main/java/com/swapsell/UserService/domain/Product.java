735ff21dcbd6d8b814aaf4e55cbe024a9663c520
vishal <vishal88kh@gmail.com>
2023-10-01 16:50:27 +0530

    added annotations for Product class

---
+++ User-Service/src/main/java/com/swapsell/UserService/domain/Product.java
@@ -0,0 +1,15 @@
+package com.swapsell.UserService.domain;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Product {
+
+}
