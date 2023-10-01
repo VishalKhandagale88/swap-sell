355f29c54594d044044e24afb9c0e0e536c4079a
vishal <vishal88kh@gmail.com>
2023-10-01 20:11:52 +0530

    added userRepository as dependency injection

--- User-Service/src/main/java/com/swapsell/UserService/service/UserServiceImpl.java
+++ User-Service/src/main/java/com/swapsell/UserService/service/UserServiceImpl.java
@@ -2,12 +2,18 @@ package com.swapsell.UserService.service;
 
 import com.swapsell.UserService.domain.User;
 import com.swapsell.UserService.exception.UserAlreadyExistsException;
+import com.swapsell.UserService.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public User registerUserToApplication(User user) throws UserAlreadyExistsException {
+
         return null;
     }
 }
