8cdb10cc9bff038825a329c1a08cfe13505c4f2d
vishal <vishal88kh@gmail.com>
2023-10-01 16:19:16 +0530

    added logic to store the jwt token in session

--- product-webapp/webapp/swap-sell/src/app/services/auth.service.ts
+++ product-webapp/webapp/swap-sell/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
+import { User } from '../modals/User';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,27 @@ export class AuthService {
     return of(this.isLoggedIn);
   }
 
-  
+  login(user:User):Observable<boolean>{
+    return this.httpClient.post(this.URL+"/login",user).pipe(map(data=>{
+      console.log(data);
+      let json = JSON.stringify(data);
+      this.jwt= JSON.parse(json).token;
+      this.message = JSON.parse(json).email;
+      let email = JSON.parse(json).email;
+
+      console.log(this.jwt,this.message);
+
+      sessionStorage.setItem("jwt",this.jwt);
+      sessionStorage.setItem("message",this.message);
+      if(this.message === "Login Successful"){
+        this.isLoggedIn = true;
+
+        // featch user data here
+
+      }else{
+        this.isLoggedIn=false
+      }
+      return this.isLoggedIn;
+    }))
+  }
 }
