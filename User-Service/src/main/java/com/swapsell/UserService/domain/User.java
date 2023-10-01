8e0ad6991fc331c54a65107ddbf7732576f3daae
vishal <vishal88kh@gmail.com>
2023-10-01 16:44:59 +0530

    added lombok annotations

--- User-Service/src/main/java/com/swapsell/UserService/domain/User.java
+++ User-Service/src/main/java/com/swapsell/UserService/domain/User.java
@@ -1,4 +1,14 @@
 package com.swapsell.UserService.domain;
 
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Data
+@ToString
+@EqualsAndHashCode
+@Document
 public class User {
 }
