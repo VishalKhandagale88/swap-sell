e3ef8a29f2871625b3e81ad00b01b40ab972d48e
vishal <vishal88kh@gmail.com>
2023-10-02 18:49:13 +0530

    added @Autowired annotation

--- User-Service/src/main/java/com/swapsell/UserService/controller/UserController.java
+++ User-Service/src/main/java/com/swapsell/UserService/controller/UserController.java
@@ -3,6 +3,7 @@ package com.swapsell.UserService.controller;
 import com.swapsell.UserService.domain.User;
 import com.swapsell.UserService.exception.UserAlreadyExistsException;
 import com.swapsell.UserService.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @Autowired
     private UserService userService;
 
     @PostMapping("user/register")
