1f447baa44eac886114fb1fa9b31ff4831cae928
vishal <vishal88kh@gmail.com>
2023-10-02 18:39:07 +0530

    added a method which will save data of user in mongo db

--- User-Service/src/main/java/com/swapsell/UserService/controller/UserController.java
+++ User-Service/src/main/java/com/swapsell/UserService/controller/UserController.java
@@ -1,9 +1,24 @@
 package com.swapsell.UserService.controller;
 
+import com.swapsell.UserService.domain.User;
+import com.swapsell.UserService.exception.UserAlreadyExistsException;
 import com.swapsell.UserService.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
     private UserService userService;
+
+    @PostMapping("user/register")
+    public ResponseEntity<?> registerUser(User user){
+        try {
+            User registeredUserToApplication = userService.registerUserToApplication(user);
+            return new ResponseEntity<>(registeredUserToApplication,HttpStatus.CREATED);
+        } catch (UserAlreadyExistsException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
 }
