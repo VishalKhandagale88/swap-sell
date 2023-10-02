f88aace1cf88e34fbb841137ec5000c56ff03b81
vishal <vishal88kh@gmail.com>
2023-10-02 18:34:44 +0530

    added UserService as dependency injection

--- User-Service/src/main/java/com/swapsell/UserService/controller/UserController.java
+++ User-Service/src/main/java/com/swapsell/UserService/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.swapsell.UserService.controller;
 
+import com.swapsell.UserService.service.UserService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    private UserService userService;
 }
