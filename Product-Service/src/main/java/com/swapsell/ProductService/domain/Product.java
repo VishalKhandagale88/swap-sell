429e62924acff9256d6f3fd1c4b83331996a5b6b
vishal <vishal88kh@gmail.com>
2023-10-09 01:12:10 +0530

    added all require annotations which is required for a class from lombok

---
+++ Product-Service/src/main/java/com/swapsell/ProductService/domain/Product.java
@@ -0,0 +1,12 @@
+package com.swapsell.ProductService.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Product {
+}
