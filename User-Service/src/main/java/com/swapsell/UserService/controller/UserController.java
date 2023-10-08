0c6939e8770b45cfe6df94628a7854fcaf1d94f2
vishal <vishal88kh@gmail.com>
2023-10-08 19:22:34 +0530

    added a method api so that client will send the request to the server and data will be removed

--- User-Service/src/main/java/com/swapsell/UserService/controller/UserController.java
+++ User-Service/src/main/java/com/swapsell/UserService/controller/UserController.java
@@ -2,6 +2,7 @@ package com.swapsell.UserService.controller;
 
 import com.swapsell.UserService.domain.Product;
 import com.swapsell.UserService.domain.User;
+import com.swapsell.UserService.exception.ProductsDoesNotExistsException;
 import com.swapsell.UserService.exception.UserAlreadyExistsException;
 import com.swapsell.UserService.exception.UserDoesNotExistsException;
 import com.swapsell.UserService.service.UserService;
@@ -64,6 +65,16 @@ public class UserController {
             User user = userService.postAnAdd(email, product);
             return new ResponseEntity<>(user,HttpStatus.OK);
         } catch (UserDoesNotExistsException e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
+        }
+    }
+
+    @PostMapping("user/product/{email}/{productId}")
+    public ResponseEntity<?> removeProducts(@PathVariable("email") String email,@PathVariable("productId") Long productId){
+        try {
+            User removedProductsFromProductsList = userService.removeProductsFromProductsList(email, productId);
+            return new ResponseEntity<>(removedProductsFromProductsList,HttpStatus.OK);
+        } catch (UserDoesNotExistsException | ProductsDoesNotExistsException e) {
             return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
         }
     }
