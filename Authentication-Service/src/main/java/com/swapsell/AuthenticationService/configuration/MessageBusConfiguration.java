7133fa8499dc603ee8017c1e92c7d54efaf2aed3
vishal <vishal88kh@gmail.com>
2023-10-09 17:02:34 +0530

    added queue and routing key

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/configuration/MessageBusConfiguration.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/configuration/MessageBusConfiguration.java
@@ -5,5 +5,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MessageBusConfiguration {
     public static final String exchangeName1 = "swap-sell-user-service-exchange";
+    public static final String queueName1 = "swap-sell-user-service-queue";
+    public static final String routingKey1 = "swap-sell-ser-service-routing-key";
 
 }
