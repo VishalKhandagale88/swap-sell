8bf326ae68ce3c7f8ca3cdb499df34f80e02f03a
vishal <vishal88kh@gmail.com>
2023-10-09 17:48:03 +0530

    added directExchange method

--- User-Service/src/main/java/com/swapsell/UserService/configuration/MessageConfiguration.java
+++ User-Service/src/main/java/com/swapsell/UserService/configuration/MessageConfiguration.java
@@ -1,5 +1,6 @@
 package com.swapsell.UserService.configuration;
 
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,4 +10,8 @@ public class MessageConfiguration {
     public static final String queueName1 = "swap-sell-user-service-queue";
     public static final String routingKey1 = "swap-sell-user-service-routing-key";
 
+    @Bean
+    public DirectExchange directExchange(){
+        return new DirectExchange(exchangeName1);
+    }
 }
