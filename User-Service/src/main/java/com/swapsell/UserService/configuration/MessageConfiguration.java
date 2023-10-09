76b56c27025ed9256edededf80129e7542e939d3
vishal <vishal88kh@gmail.com>
2023-10-09 17:43:21 +0530

    added exchange

--- User-Service/src/main/java/com/swapsell/UserService/configuration/MessageConfiguration.java
+++ User-Service/src/main/java/com/swapsell/UserService/configuration/MessageConfiguration.java
@@ -4,4 +4,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MessageConfiguration {
+    public static final String exchangeName1 = "swap-sell-user-service-exchange";
+
 }
