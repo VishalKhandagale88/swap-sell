b8dceff0bb4982ddd73f799dbf5b093e0ff77de5
vishal <vishal88kh@gmail.com>
2023-10-09 17:55:38 +0530

    added userJackson2JsonMessage and rabbitTemplate method

--- User-Service/src/main/java/com/swapsell/UserService/configuration/MessageConfiguration.java
+++ User-Service/src/main/java/com/swapsell/UserService/configuration/MessageConfiguration.java
@@ -1,6 +1,9 @@
 package com.swapsell.UserService.configuration;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,5 +25,16 @@ public class MessageConfiguration {
     @Bean
     public Binding binding(Queue queue, DirectExchange directExchange){
         return BindingBuilder.bind(queue).to(directExchange).with(routingKey1);
+    }
+    @Bean
+    public Jackson2JsonMessageConverter userJackson2JsonMessage() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(userJackson2JsonMessage());
+        return rabbitTemplate;
     }
 }
