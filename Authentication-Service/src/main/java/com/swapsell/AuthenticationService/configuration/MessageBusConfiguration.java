27d2f50539501e7ba618c854c97d145c581beab6
vishal <vishal88kh@gmail.com>
2023-10-09 17:11:20 +0530

    added ampq dependency

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/configuration/MessageBusConfiguration.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/configuration/MessageBusConfiguration.java
@@ -1,5 +1,6 @@
 package com.swapsell.AuthenticationService.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -7,5 +8,4 @@ public class MessageBusConfiguration {
     public static final String exchangeName1 = "swap-sell-user-service-exchange";
     public static final String queueName1 = "swap-sell-user-service-queue";
     public static final String routingKey1 = "swap-sell-ser-service-routing-key";
-
 }
