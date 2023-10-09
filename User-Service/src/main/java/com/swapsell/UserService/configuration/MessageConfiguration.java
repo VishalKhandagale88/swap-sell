8b374086a5573c0c686e258a977404029f7430a6
vishal <vishal88kh@gmail.com>
2023-10-09 17:44:01 +0530

    added queue

--- User-Service/src/main/java/com/swapsell/UserService/configuration/MessageConfiguration.java
+++ User-Service/src/main/java/com/swapsell/UserService/configuration/MessageConfiguration.java
@@ -5,5 +5,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MessageConfiguration {
     public static final String exchangeName1 = "swap-sell-user-service-exchange";
+    public static final String queueName1 = "swap-sell-user-service-queue";
 
 }
