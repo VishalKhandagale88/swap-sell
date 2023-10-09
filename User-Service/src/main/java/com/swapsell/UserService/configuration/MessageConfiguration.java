f807a220b38734ae192af84d4381ebf7cb18cae0
vishal <vishal88kh@gmail.com>
2023-10-09 17:46:39 +0530

    added ampq dependency

--- User-Service/src/main/java/com/swapsell/UserService/configuration/MessageConfiguration.java
+++ User-Service/src/main/java/com/swapsell/UserService/configuration/MessageConfiguration.java
@@ -1,5 +1,6 @@
 package com.swapsell.UserService.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
