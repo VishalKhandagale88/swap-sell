ac35ec0d4a27ae3a0ef9135b09e36db07179af84
vishal <vishal88kh@gmail.com>
2023-10-09 17:13:23 +0530

    added a method which will return the direct exchage

--- Authentication-Service/src/main/java/com/swapsell/AuthenticationService/configuration/MessageBusConfiguration.java
+++ Authentication-Service/src/main/java/com/swapsell/AuthenticationService/configuration/MessageBusConfiguration.java
@@ -1,11 +1,18 @@
 package com.swapsell.AuthenticationService.configuration;
 
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class MessageBusConfiguration {
     public static final String exchangeName1 = "swap-sell-user-service-exchange";
     public static final String queueName1 = "swap-sell-user-service-queue";
     public static final String routingKey1 = "swap-sell-ser-service-routing-key";
+    @Bean
+    @Primary
+    public DirectExchange directExchange(){
+        return new DirectExchange(exchangeName1);
+    }
 }
