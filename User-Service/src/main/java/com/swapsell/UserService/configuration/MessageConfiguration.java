c7cca17755675f28ee01442345bafb9196f220a4
vishal <vishal88kh@gmail.com>
2023-10-09 17:44:31 +0530

    added routing key

--- User-Service/src/main/java/com/swapsell/UserService/configuration/MessageConfiguration.java
+++ User-Service/src/main/java/com/swapsell/UserService/configuration/MessageConfiguration.java
@@ -6,5 +6,6 @@ import org.springframework.context.annotation.Configuration;
 public class MessageConfiguration {
     public static final String exchangeName1 = "swap-sell-user-service-exchange";
     public static final String queueName1 = "swap-sell-user-service-queue";
+    public static final String routingKey1 = "swap-sell-user-service-routing-key";
 
 }
