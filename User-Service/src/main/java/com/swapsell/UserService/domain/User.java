9b784a9ffb0cd71421a4b9e36668493da37d2bbb
vishal <vishal88kh@gmail.com>
2023-10-10 17:57:48 +0530

    added id again because it is causing bug while saving the user details after updating

--- User-Service/src/main/java/com/swapsell/UserService/domain/User.java
+++ User-Service/src/main/java/com/swapsell/UserService/domain/User.java
@@ -15,6 +15,8 @@ import java.util.List;
 @EqualsAndHashCode
 @Document
 public class User {
+    @MongoId
+    private String id;
     private String firstName;
     private String lastName;
     private String phoneNumber;
