95cf093c3737486e7f2d5dc371d0a0107021aa93
vishal <vishal88kh@gmail.com>
2023-10-18 14:13:19 +0530

    changed the logic of method

--- User-Service/src/main/java/com/swapsell/UserService/service/UserServiceImpl.java
+++ User-Service/src/main/java/com/swapsell/UserService/service/UserServiceImpl.java
@@ -11,14 +11,16 @@ import com.swapsell.UserService.repository.UserRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 import java.util.*;
-import java.util.Random;
+
 @Service
 public class UserServiceImpl implements UserService {
+    private static final Random random = new Random();
     @Autowired
     private UserRepository userRepository;
-    private static final Random random = new Random();
+
     public static long generateId() {
         long id = random.nextLong();
         if (id == Long.MIN_VALUE) {
@@ -30,80 +32,63 @@ public class UserServiceImpl implements UserService {
     }
 
     @RabbitListener(queues = MessageConfiguration.queueName1)
-    public void userDataFromAuthService(UserDTO user) throws UserAlreadyExistsException{
+    public void userDataFromAuthService(UserDTO user) throws UserAlreadyExistsException {
         String userEmail = user.getEmail();
         String userLastName = user.getLastName();
         String userFirstName = user.getFirstName();
-        User newUser = new User(generateId(),userFirstName,userLastName,null,userEmail,null,null,null,0,null,null,null);
+        User newUser = new User(generateId(), userFirstName, userLastName, null, userEmail, null, null, null, 0, null, null, null);
         registerUserToApplication(newUser);
     }
+
     @Override
     public User registerUserToApplication(User user) throws UserAlreadyExistsException {
         Optional<User> userByEmail = userRepository.findUserByEmail(user.getEmail());
-        if (userByEmail.isPresent()){
-            throw new UserAlreadyExistsException("user with "+user.getEmail()+" is already present");
+        if (userByEmail.isPresent()) {
+            throw new UserAlreadyExistsException("user with " + user.getEmail() + " is already present");
         }
         return userRepository.save(user);
     }
 
     @Override
-    public User updateUserData(User user,String email) throws UserDoesNotExistsException {
+    public User updateUserData(User user, String email) throws UserDoesNotExistsException {
         Optional<User> userByEmail = userRepository.findUserByEmail(email);
-        User existingUser ;
-        if (userByEmail.isPresent()){
-             existingUser = userByEmail.get();
-            if (user.getAddress()!=null){
-                existingUser.setAddress(user.getAddress());
-            }
-            if (user.getFirstName()!=null){
-                existingUser.setFirstName(user.getFirstName());
-            }
-            if (user.getLastName()!=null){
-                existingUser.setLastName(user.getLastName());
-            }
-            if (user.getPhoneNumber()!=null){
-                existingUser.setPhoneNumber(user.getPhoneNumber());
-            }
-            if (user.getCity()!=null){
-                existingUser.setCity(user.getCity());
-            }
-            if (user.getPinCode()!=0){
-                existingUser.setPinCode(user.getPinCode());
-            }
-            if (user.getGender()!=null){
-                existingUser.setGender(user.getGender());
-            }
-            if (user.getImage()!=null){
-                existingUser.setImage(user.getImage());
-            }
-            userRepository.save(existingUser);
-
-        }else {
-            throw new UserDoesNotExistsException("user with "+user.getEmail()+" does not exists");
+        if (userByEmail.isPresent()) {
+            User userFromDb = userByEmail.get();
+            userFromDb.setFirstName(user.getFirstName());
+            userFromDb.setLastName(user.getLastName());
+            userFromDb.setPhoneNumber(user.getPhoneNumber());
+            userFromDb.setAddress(user.getAddress());
+            userFromDb.setCity(user.getCity());
+            userFromDb.setState(user.getState());
+            userFromDb.setPinCode(user.getPinCode());
+            userFromDb.setGender(user.getGender());
+            userFromDb.setImage(user.getImage());
+            userFromDb.setProducts(user.getProducts());
+            return userRepository.save(userFromDb);
         }
+        throw new UserDoesNotExistsException("No user exists with email id " + email);
 
-        return existingUser;
 
     }
 
     @Override
     public User getUserInformation(String email) throws UserDoesNotExistsException {
         Optional<User> userByEmail = userRepository.findUserByEmail(email);
-        if (userByEmail.isPresent()){
+        if (userByEmail.isPresent()) {
             return userByEmail.get();
-        }else {
-            throw new UserDoesNotExistsException("User with "+email+" is not found");
+        } else {
+            throw new UserDoesNotExistsException("User with " + email + " is not found");
         }
     }
 
     @Override
     public boolean removeUserFromDb(String emailId) throws UserDoesNotExistsException {
         Optional<User> userByEmail = userRepository.findUserByEmail(emailId);
-        if (userByEmail.isPresent()){
+        if (userByEmail.isPresent()) {
             userRepository.delete(userByEmail.get());
             return true;
-        }else {
-            throw new UserDoesNotExistsException("user does not exists with "+emailId+" this email");
+        } else {
+            throw new UserDoesNotExistsException("user does not exists with " + emailId + " this email");
         }
     }
 
@@ -113,18 +98,18 @@ public class UserServiceImpl implements UserService {
         User user;
         LocalDateTime localDateTime = LocalDateTime.now();
         product.setAddPostedOnDate(localDateTime);
-        if (userByEmail.isPresent()){
+        if (userByEmail.isPresent()) {
             user = userByEmail.get();
             List<Product> products = user.getProducts();
-            if (products==null){
+            if (products == null) {
                 // will create a product list
                 user.setProducts(Collections.singletonList(product));
-            }else {
+            } else {
                 products.add(product);
                 user.setProducts(products);
             }
-        }else {
-            throw new UserDoesNotExistsException("User does not found with "+emailId+" id");
+        } else {
+            throw new UserDoesNotExistsException("User does not found with " + emailId + " id");
         }
         userRepository.save(user);
         return user;
@@ -135,18 +120,18 @@ public class UserServiceImpl implements UserService {
     public User removeProductsFromProductsList(String emailId, Long productId) throws UserDoesNotExistsException, ProductsDoesNotExistsException {
         Optional<User> userByEmail = userRepository.findUserByEmail(emailId);
         User user;
-        if (userByEmail.isPresent()){
+        if (userByEmail.isPresent()) {
             user = userByEmail.get();
             List<Product> productList = user.getProducts();
             Optional<Product> optionalProduct = productList.stream().filter(searchItem -> Objects.equals(searchItem.getId(), productId)).findAny();
-            if (optionalProduct.isPresent()){
+            if (optionalProduct.isPresent()) {
                 productList.remove(optionalProduct.get());
                 user.setProducts(productList);
-            }else{
-                throw new ProductsDoesNotExistsException("Product is not available with "+productId);
+            } else {
+                throw new ProductsDoesNotExistsException("Product is not available with " + productId);
             }
-        }else {
-            throw new UserDoesNotExistsException("No user found with "+emailId);
+        } else {
+            throw new UserDoesNotExistsException("No user found with " + emailId);
         }
         userRepository.save(user);
         return user;
