95f3c5cb1506f691c557f017517e700aee16d65a
vishal <vishal88kh@gmail.com>
2023-10-19 17:03:32 +0530

    added a method to get the product id

--- User-Service/src/main/java/com/swapsell/UserService/repository/ProductRepository.java
+++ User-Service/src/main/java/com/swapsell/UserService/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package com.swapsell.UserService.repository;
 import com.swapsell.UserService.domain.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ProductRepository extends MongoRepository<Product,String> {
+import java.util.Optional;
 
+public interface ProductRepository extends MongoRepository<Product,String> {
+    Optional<Product> findProductById(String id);
 }
