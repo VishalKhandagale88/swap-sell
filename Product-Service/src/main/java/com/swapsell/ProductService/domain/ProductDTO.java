2788ceb53183a9f034f45997eefe67906417158a
vishal <vishal88kh@gmail.com>
2023-10-29 16:49:22 +0530

    changed type of pinCode and added a field called condition

--- Product-Service/src/main/java/com/swapsell/ProductService/domain/ProductDTO.java
+++ Product-Service/src/main/java/com/swapsell/ProductService/domain/ProductDTO.java
@@ -1,10 +1,7 @@
 package com.swapsell.ProductService.domain;
 
 import jakarta.websocket.server.ServerEndpoint;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.lang.model.element.NestingKind;
 import java.util.List;
@@ -13,18 +10,20 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
+@Data
 public class ProductDTO {
     private String name;
     private String title;
     private String description;
     private Double price;
     private  String category;
-    private  String ageInDays;
+    private String condition;
+    private  Integer ageInDays;
     private String email;
     private  String address;
     private String city;
     private String state;
-    private  String pinCode;
+    private  Long pinCode;
     private String date;
     private List<String> images;
 
