09b219013f939c72cf8f56a8967fcc555bbd9739
vishal <vishal88kh@gmail.com>
2023-10-29 16:39:24 +0530

    added queueName , exchangeName and routing key

--- Product-Service/src/main/java/com/swapsell/ProductService/configuration/MessageConfiguration.java
+++ Product-Service/src/main/java/com/swapsell/ProductService/configuration/MessageConfiguration.java
@@ -4,5 +4,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MessageConfiguration {
+    public static final String queueName1 = "swap-sell-user-service-queue";
+    public static final String exchangeName2 = "second-service-exchange";
+    public static final String routingKey2 = "second-service-routing-key";
 
 }
