1da498ac194a57f76283dc5f3f04adee3fb30c6b
vishal <vishal88kh@gmail.com>
2023-10-29 16:42:22 +0530

    added binding method to bind with queue nad exchange

--- Product-Service/src/main/java/com/swapsell/ProductService/configuration/MessageConfiguration.java
+++ Product-Service/src/main/java/com/swapsell/ProductService/configuration/MessageConfiguration.java
@@ -1,5 +1,10 @@
 package com.swapsell.ProductService.configuration;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -7,5 +12,10 @@ public class MessageConfiguration {
     public static final String queueName1 = "swap-sell-user-service-queue";
     public static final String exchangeName2 = "second-service-exchange";
     public static final String routingKey2 = "second-service-routing-key";
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey2);
+    }
 
 }
