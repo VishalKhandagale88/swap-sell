ebca5f68eb6c8b6ec4fa7de66dec718d99da78f5
vishal <vishal88kh@gmail.com>
2023-11-11 17:20:31 +0530

    removed the old addNewProducts method

--- Product-Service/src/main/java/com/swapsell/ProductService/service/ProductServiceImpl.java
+++ Product-Service/src/main/java/com/swapsell/ProductService/service/ProductServiceImpl.java
@@ -21,31 +21,6 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
     @Override
-    public Product addNewProduct(ProductDTO productDTO) {
-        Product product1 = new Product();
-        product1.setName(productDTO.getName());
-        product1.setTitle(productDTO.getTitle());
-        product1.setDescription(productDTO.getDescription());
-        product1.setCategory(productDTO.getCategory());
-        product1.setPrice(productDTO.getPrice());
-        product1.setAgeInDays(productDTO.getAgeInDays());
-        product1.setCondition(productDTO.getCondition());
-        product1.setAddress(productDTO.getAddress());
-        product1.setCity(productDTO.getCity());
-        product1.setState(productDTO.getState());
-        product1.setPinCode(productDTO.getPinCode());
-        product1.setDate(productDTO.getDate());
-        product1.setImages(productDTO.getImages());
-
-        Product newProduct = productRepository.save(product1);
-
-        userRepository.createOwnsRelationship(productDTO.getEmail(), newProduct.getId());
-
-        return newProduct;
-
-    }
-
-    @Override
     public User addNewUser(User user) {
         User user1 = userRepository.save(user);
         return user1;
