362d612cfffe2a03acf1952b31913e3b6393308d
vishal <vishal88kh@gmail.com>
2023-11-11 17:24:43 +0530

    added addNewProduct method

--- Product-Service/src/main/java/com/swapsell/ProductService/contoller/ProductController.java
+++ Product-Service/src/main/java/com/swapsell/ProductService/contoller/ProductController.java
@@ -1,25 +1,32 @@
 package com.swapsell.ProductService.contoller;
 
+import com.swapsell.ProductService.domain.Product;
+import com.swapsell.ProductService.domain.ProductDTO;
 import com.swapsell.ProductService.domain.User;
 import com.swapsell.ProductService.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
     @Autowired
     private final ProductService productService;
-
-
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
-
     @PostMapping("/user")
     public User addNewUser(@RequestBody User user){
         return productService.addNewUser(user);
-
+    }
+    @PostMapping("/products")
+    public ResponseEntity<?> addNewProduct(@RequestBody List<ProductDTO> productDTOList){
+        List<Product> products = productService.addNewProduct(productDTOList);
+        return new ResponseEntity<>(products,HttpStatus.OK);
     }
 }
